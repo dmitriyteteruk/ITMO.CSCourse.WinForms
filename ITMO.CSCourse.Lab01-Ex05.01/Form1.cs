d8bc6fc06a921b825cf0a6af82ed18267900ad21
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2022-01-13 10:56:58 +0300

    Lab01-Ex05.01 added

---
+++ ITMO.CSCourse.Lab01-Ex05.01/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.CSCourse.Lab01_Ex05._01
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			System.Drawing.Drawing2D.GraphicsPath myPath = new System.Drawing.Drawing2D.GraphicsPath();
+			myPath.AddEllipse(0, 0, this.Width, this.Height);  // добавляем элипс с размерами формы
+			Region myRegion = new Region(myPath);
+			this.Region = myRegion;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			this.Close();		// по нажатию кнопки закрываем 
+		}
+	}
+}
