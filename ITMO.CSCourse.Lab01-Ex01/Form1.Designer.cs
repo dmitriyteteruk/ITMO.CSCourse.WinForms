b2a8d98b7ce1c174452adc231dd446e4299af92b
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2022-01-13 09:40:55 +0300

    Add project files.

---
+++ ITMO.CSCourse.Lab01-Ex01/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿/*
+ 12. Измените поочередно расположение левой и верхней границ формы и сравните
+поведение кнопок внутри формы. Обратите внимание, что расстояние до этих границ 
+от кнопки Border Style остается постоянным. Почему?
+
+Потому что у кнопки Border Style значение Location зафиксировано координатами
+ 
+ */
+namespace ITMO.CSCourse.Lab01_Ex01
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+			this.button1.FlatAppearance.BorderSize = 3;
+			this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Location = new System.Drawing.Point(54, 61);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(187, 75);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Border Style";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+			this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+			this.button2.FlatAppearance.BorderSize = 3;
+			this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Location = new System.Drawing.Point(305, 61);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(187, 75);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Resize";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+			this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Fuchsia;
+			this.button3.FlatAppearance.BorderSize = 3;
+			this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button3.ForeColor = System.Drawing.Color.Black;
+			this.button3.Location = new System.Drawing.Point(555, 61);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(187, 75);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "Opacity";
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(811, 207);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Location = new System.Drawing.Point(200, 100);
+			this.Name = "Form1";
+			this.Opacity = 0.9D;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+			this.Text = "Trey Research";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+	}
+}
+
