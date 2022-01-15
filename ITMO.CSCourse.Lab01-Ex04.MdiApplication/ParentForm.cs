65343660745ae380ddce96d8b37edfa22d1c1ac8
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2022-01-15 13:22:25 +0300

    Lab02-Ex03 added

--- ITMO.CSCourse.Lab01-Ex04.MdiApplication/ParentForm.cs
+++ ITMO.CSCourse.Lab01-Ex04.MdiApplication/ParentForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// в этом коде есть Lab01-04 и Lab02-03.
+// код Lab02-03 дополнительно прокоментирован
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -45,6 +47,26 @@ namespace ITMO.CSCourse.Lab01_Ex04.MdiApplication
 		private void ParentForm_Load(object sender, EventArgs e)
 		{
 
+		}
+
+		// ниже код из Lab02-Ex03
+		private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+		{
+			switch (e.ClickedItem.Tag.ToString())
+			{
+				case "NewDoc":
+					ChildForm newChild = new ChildForm();
+					newChild.MdiParent = this;
+					newChild.Show();
+					newChild.Text = newChild.Text + " " + ++openDocuments;
+					break;
+					case "Cascade":
+					this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+					break;
+					case "Title":
+					this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+					break;
+			}
 		}
 	}
 }
