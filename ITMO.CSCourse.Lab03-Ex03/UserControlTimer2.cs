f570e24111d3db9f4bff027382bb82b24ccee8f7
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2022-01-18 17:21:05 +0300

    Lab03-Ex02 added

---
+++ ITMO.CSCourse.Lab03-Ex03/UserControlTimer2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.CSCourse.Lab03_Ex03
+{
+	public partial class UserControlTimer2 : Control
+	{
+		public UserControlTimer2()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnPaint(PaintEventArgs pe)
+		{
+			base.OnPaint(pe);
+			Graphics g = pe.Graphics;
+			g.FillRectangle(Brushes.White, 0, 0, this.Width, this.Height); // Brushes.Blue плохо читается, заменил на White
+
+			pe.Graphics.DrawString(DateTime.Now.ToLongTimeString(), this.Font, new SolidBrush(this.ForeColor), 0, 0);
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			this.Refresh();
+
+		}
+	}
+}
