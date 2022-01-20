2b7b1f8ba409535cd1a2adc6651ab2bf5333fdb1
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2022-01-20 19:45:25 +0300

    WPF app with connection to local SQL added

--- IMTO.CSCourse.SQL-connection/Form3.Designer.cs
+++ IMTO.CSCourse.SQL-connection/Form3.Designer.cs
@@ -35,11 +35,11 @@ namespace IMTO.CSCourse.SQL_connection
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(71, 203);
+			this.button1.Location = new System.Drawing.Point(28, 203);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.Size = new System.Drawing.Size(165, 23);
 			this.button1.TabIndex = 0;
-			this.button1.Text = "button1";
+			this.button1.Text = "Прочитать данные из БД";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
---
+++ ITMO.CSCourse.WPF.TestApp/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace ITMO.CSCourse.WPF.TestApp
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button1_Click(object sender, RoutedEventArgs e)
+		{
+			using (SqlConnection cn = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=WindowsForms;Data Source=DESKTOP-JAMOVG3\\SQLEXPRESS"))
+			{
+				cn.Open();
+				using (SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.Gifts", cn))
+				{
+					using (SqlDataReader reader = cmd.ExecuteReader())
+					{
+						while (reader.Read())
+						{
+							lbResultData.Items.Add(reader[0] + " " + reader[1] + " " + reader[2] + reader[3] + " " + reader[4] + " " + reader[5] + " " + reader[6] + " " + reader[7]);
+						}
+					}
+				}
+			}
+		}
+
+		private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+		{
+
+		}
+
+		private void Button_CleanResultWindow_Click(object sender, RoutedEventArgs e)
+		{
+			lbResultData.Items.Clear();
+		}
+	}
+}
