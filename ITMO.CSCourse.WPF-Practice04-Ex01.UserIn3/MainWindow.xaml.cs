cec307eb67223fa8e76025c3230e6960c6a396be
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2022-01-28 18:03:10 +0300

    4.3 xaml fixed

---
+++ ITMO.CSCourse.WPF-Practice04-Ex01.UserIn3/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ITMO.CSCourse.WPF_Practice04_Ex01.UserIn3
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			foreach (FontFamily F in Fonts.SystemFontFamilies)
+			{
+				listBox1.Items.Add(F.ToString());      // ошибка в практической - comboBox1.Items.Add(F.ToString()); - меняем comboBox1 на listBox1
+			}
+
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+			{
+				richTextBox1.Selection.ApplyPropertyValue(FontWeightProperty, FontWeights.Bold);
+			}
+
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+
+			{
+				richTextBox1.Selection.ApplyPropertyValue(FontStyleProperty, FontStyles.Italic);
+			}
+
+			private void Slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+
+			{
+				richTextBox1.Selection.ApplyPropertyValue(FontSizeProperty, Slider1.Value.ToString());
+			}
+
+
+		private void listBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			try
+			{
+				richTextBox1.Selection.ApplyPropertyValue(FontFamilyProperty, ((ListBoxItem)listBox1.SelectedItem).FontFamily);
+			}
+			catch
+			{
+
+			}
+		}
+	}
+}
