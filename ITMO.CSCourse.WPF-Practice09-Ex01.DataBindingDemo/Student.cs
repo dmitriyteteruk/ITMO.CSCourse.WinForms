836d49447b62946ee960d16a6695e21084ed2267
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2022-01-30 00:54:24 +0300

    WPF Practice09 added

---
+++ ITMO.CSCourse.WPF-Practice09-Ex01.DataBindingDemo/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace ITMO.CSCourse.WPF_Practice09_Ex01.DataBindingDemo
+{
+	public class Student
+	{
+		public string StudentName { get; set; }
+		public bool IsEnrolled { get; set; }
+	}
+
+}
---
+++ ITMO.CSCourse.WPF-Practice09-Ex01.DataBindingDemo/StudentList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace ITMO.CSCourse.WPF_Practice09_Ex01.DataBindingDemo
+{
+	public class StudentList : ObservableCollection<Student>
+	{
+	
+	}
+}
