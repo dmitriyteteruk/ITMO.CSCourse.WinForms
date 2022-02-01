3725b9218591a110608e2da83cd7167bb04a29a4
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2022-02-01 19:01:43 +0300

    folders updated

---
+++ ITMO.CSCourse.WPF-Practice07-Ex03.WpfApplicationChart/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ITMO.CSCourse.WPF_Practice07_Ex03.WpfApplicationChart
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
