377b34e9cb88ffaf87bad0719772bdfe9ee338b9
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2022-02-01 14:12:29 +0300

    Added WPF Practice 07-Ex02

---
+++ ITMO.CSCourse.WPF-Practice07-Ex02.InkPad/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ITMO.CSCourse.WPF_Practice07_Ex02.InkPad
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonClear_Click(object sender, RoutedEventArgs e)
+		{
+			this.inkCanvas1.Strokes.Clear();
+		}
+
+		private void buttonClose_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
